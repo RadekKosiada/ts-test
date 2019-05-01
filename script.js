db28ced67612a42788688f79bc40daa42c0d069b
RadekKosiada <kosiadar@gmail.com>
2019-05-01 12:53:25 +0200

    Get 3 latest launches

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 
 (function () {
+    let latestLaunchesArr = [];
     fetch("https://api.spacexdata.com/v3/launches/past")
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
-            console.log(JSON.stringify(data[76]));
+        .then(function (data) {
+            let counter = 0;
+            for (let i = data.length-1; i >=0; i--) {  
+                console.log(data.length)              
+                latestLaunchesArr.push(data[i]);                
+                counter++;
+                if(counter==3) {
+                    break;
+                }
+            }
+            
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err.message);
         });
+
+        console.log(latestLaunchesArr)  
 })();
 
 
