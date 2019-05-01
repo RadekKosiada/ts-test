c764199a5dcb6677a3814c9a00cafe60e7613dd4
RadekKosiada <kosiadar@gmail.com>
2019-05-01 15:31:07 +0200

    Add basic styling & grid

--- script.js
+++ script.js
@@ -23,45 +23,62 @@
                 }
             }
             // - Mission Name, Year, Date, Rocket Name and Type, Launch Site
-            for(let i = 0; i <latestLaunchesArr.length; i++) {
-                let rocketImg = document.createElement('img');
+            let mainContent = document.createElement("div");
+            mainContent.setAttribute("id", "main-content")
+            document.body.appendChild(mainContent);
+
+            for (let i = 0; i < latestLaunchesArr.length; i++) {
+                let contentUnits = document.createElement("div");
+                contentUnits.setAttribute("class", "rocket-units");
+                mainContent.appendChild(contentUnits);
+
+                let allContentUnits = document.querySelectorAll(".rocket-units");
+                console.log(allContentUnits);
+
+                let imgDiv = document.createElement("div");
+                imgDiv.setAttribute("class", "img-div")
+                allContentUnits[i].appendChild(imgDiv);
+
+                let rocketImg = document.createElement("img");
                 rocketImg.setAttribute("src", latestLaunchesArr[i].links.flickr_images[0]);
-                document.body.appendChild(rocketImg);
+                imgDiv.appendChild(rocketImg);
 
-                let missionName = document.createElement('p');
+                let missionName = document.createElement("p");
                 missionName.innerHTML = "<span class='title'>Mission Name: </span>" + latestLaunchesArr[i].mission_name;
-                document.body.appendChild(missionName);
-               
-                let launchYear = document.createElement('p');
+                allContentUnits[i].appendChild(missionName);
+
+                let launchYear = document.createElement("p");
                 launchYear.innerHTML = "<span class='title'>Launch Year: </span>" + latestLaunchesArr[i].launch_year;
-                document.body.appendChild(launchYear);
+                allContentUnits[i].appendChild(launchYear);
 
-                let launchDate =  latestLaunchesArr[i].launch_date_local.slice(0, 10);
-                let launchDateHtml = document.createElement('p');
+                let launchDate = latestLaunchesArr[i].launch_date_local.slice(0, 10);
+                let launchDateHtml = document.createElement("p");
                 launchDateHtml.innerHTML = "<span class='title'>Date: </span>" + launchDate;
-                document.body.appendChild(launchDateHtml);
+                allContentUnits[i].appendChild(launchDateHtml);
 
-                let rocket = document.createElement('p');
-                rocket.innerHTML = "<span class='title'>Rocket Name: </span>" +  latestLaunchesArr[i].rocket.rocket_name + " <span class='title'>Type: </span>" +  latestLaunchesArr[i].rocket.rocket_type;
-                document.body.appendChild(rocket);
-               
-                let launchSite = document.createElement('p');
+                let rocket = document.createElement("p");
+                rocket.innerHTML = "<span class='title'>Rocket Name: </span>" + latestLaunchesArr[i].rocket.rocket_name + " <span class='title'>Type: </span>" + latestLaunchesArr[i].rocket.rocket_type;
+                allContentUnits[i].appendChild(rocket);
+
+                let launchSite = document.createElement("p");
                 launchSite.innerHTML = "<span class='title'>Launch Site: </span>" + latestLaunchesArr[0].launch_site.site_name_long;
-                document.body.appendChild(launchSite);
-            }
-            
+                allContentUnits[i].appendChild(launchSite);
 
-            
+
+            }
+
+
+
             console.log(
                 latestLaunchesArr[0],
                 latestLaunchesArr[0].mission_name,
                 latestLaunchesArr[0].launch_year,
-               
+
                 latestLaunchesArr[0].rocket.rocket_name,
                 latestLaunchesArr[0].rocket.rocket_type,
                 latestLaunchesArr[0].launch_site.site_name_long,
                 latestLaunchesArr[0].links.flickr_images[0]
-                )
+            )
         })
         .catch(function (err) {
             console.log(err.message);
