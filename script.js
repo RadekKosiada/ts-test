a54d8045f229f4bb56296fbfbab858319cc1ae47
RadekKosiada <kosiadar@gmail.com>
2019-05-01 12:57:38 +0200

    Comment the code

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 
 (function () {
+    //defining an empty array to push latest launches
     let latestLaunchesArr = [];
     fetch("https://api.spacexdata.com/v3/launches/past")
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            //counter to limit the launches
             let counter = 0;
-            for (let i = data.length-1; i >=0; i--) {  
-                console.log(data.length)              
-                latestLaunchesArr.push(data[i]);                
+            //looping through the data array, backwards as we want the latest (last) launches
+            for (let i = data.length - 1; i >= 0; i--) {
+                //using push method to add the latest launches to the array
+                latestLaunchesArr.push(data[i]);
+                // with each push we increase counter by 1
                 counter++;
-                if(counter==3) {
+                //we break the for loop, when the counter equals 3
+                if (counter == 3) {
                     break;
                 }
             }
-            
         })
         .catch(function (err) {
             console.log(err.message);
         });
 
-        console.log(latestLaunchesArr)  
+    console.log(latestLaunchesArr)
 })();
 
 
