ee15eea7f652bf537ff1a124034adbee00629cbd
RadekKosiada <kosiadar@gmail.com>
2019-05-01 12:31:34 +0200

    Wrap fetch in iife

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-fetch("https://api.spacexdata.com/v3/launches/past")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(JSON.stringify(data));
-    })
-    .catch(function(err) {
-        console.log(err.message);
-    });
+
+(function () {
+    fetch("https://api.spacexdata.com/v3/launches/past")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(JSON.stringify(data[76]));
+        })
+        .catch(function(err) {
+            console.log(err.message);
+        });
+})();
+
 
