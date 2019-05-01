9df30f3b5e14562e31dfd0e0a2005e12380f0e20
RadekKosiada <kosiadar@gmail.com>
2019-05-01 20:08:18 +0200

    Render data in DOM

--- script-vue.js
+++ script-vue.js
@@ -1,17 +1,27 @@
 var app = new Vue({
   el: '#app',
   data: {
-    
+    lastLaunches: [],
+    limitNumber: 3,
+    counter: 0,
+
   },
   mounted() {
     axios
       .get('https://api.spacexdata.com/v3/launches/past')
-      .then(function(response){
-        console.log("OK", response.data)
+      .then(response => {
+        console.log("OK", response.data);
+        for (let i = response.data.length - 1; i >= 0; i--) {
+          this.lastLaunches.push(response.data[i]);
+          this.counter++;
+          if (this.counter == this.limitNumber) {
+            break;
+          }
+          
+        }
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err.message);
       });
-    
   }
 })
