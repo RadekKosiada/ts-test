25e322fe5652c899cda5e3204d61d655ce46f23e
RadekKosiada <kosiadar@gmail.com>
2019-05-01 12:23:20 +0200

    Fetch the past launches from 'spacexdata'

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ fetch("https://api.spacexdata.com/v3/launches/past")
         return response.json();
     })
     .then(function(data) {
-        console.log(JSON.stringify(data[76]));
-    })
-    .catch(function(err) {
-        console.log(err.message);
-    });
-
-fetch("https://api.spacexdata.com/v3/launches/latest")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
         console.log(JSON.stringify(data));
     })
     .catch(function(err) {
