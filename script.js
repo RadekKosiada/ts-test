3e74efdb9fba98deb4cb90fa078c4a1f84bb6376
RadekKosiada <kosiadar@gmail.com>
2019-05-01 14:33:35 +0200

    Render the data in the DOM

--- script.js
+++ script.js
@@ -16,20 +16,47 @@
                 latestLaunchesArr.push(data[i]);
                 // with each push we increase counter by 1
                 counter++;
-                //we break the for loop, when the counter equals 3
+                //we break out of the for loop, when the counter equals 3
                 if (counter === 3) {
                     break;
 
                 }
             }
             // - Mission Name, Year, Date, Rocket Name and Type, Launch Site
+            for(let i = 0; i <latestLaunchesArr.length; i++) {
+                let rocketImg = document.createElement('img');
+                rocketImg.setAttribute("src", latestLaunchesArr[i].links.flickr_images[0]);
+                document.body.appendChild(rocketImg);
 
-            let launchData =  latestLaunchesArr[0].launch_date_local.slice(0, 10);
+                let missionName = document.createElement('p');
+                missionName.innerHTML = "<span class='title'>Mission Name: </span>" + latestLaunchesArr[i].mission_name;
+                document.body.appendChild(missionName);
+               
+                let launchYear = document.createElement('p');
+                launchYear.innerHTML = "<span class='title'>Launch Year: </span>" + latestLaunchesArr[i].launch_year;
+                document.body.appendChild(launchYear);
+
+                let launchDate =  latestLaunchesArr[i].launch_date_local.slice(0, 10);
+                let launchDateHtml = document.createElement('p');
+                launchDateHtml.innerHTML = "<span class='title'>Date: </span>" + launchDate;
+                document.body.appendChild(launchDateHtml);
+
+                let rocket = document.createElement('p');
+                rocket.innerHTML = "<span class='title'>Rocket Name: </span>" +  latestLaunchesArr[i].rocket.rocket_name + " <span class='title'>Type: </span>" +  latestLaunchesArr[i].rocket.rocket_type;
+                document.body.appendChild(rocket);
+               
+                let launchSite = document.createElement('p');
+                launchSite.innerHTML = "<span class='title'>Launch Site: </span>" + latestLaunchesArr[0].launch_site.site_name_long;
+                document.body.appendChild(launchSite);
+            }
+            
+
+            
             console.log(
                 latestLaunchesArr[0],
                 latestLaunchesArr[0].mission_name,
                 latestLaunchesArr[0].launch_year,
-                launchData,
+               
                 latestLaunchesArr[0].rocket.rocket_name,
                 latestLaunchesArr[0].rocket.rocket_type,
                 latestLaunchesArr[0].launch_site.site_name_long,
