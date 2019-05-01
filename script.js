3e29eb7132a5366f8944a55a83f316e282047653
RadekKosiada <kosiadar@gmail.com>
2019-05-01 13:38:28 +0200

    Analyze all needed data + img

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
             return response.json();
         })
         .then(function (data) {
+
             //counter to limit the launches
             let counter = 0;
             //looping through the data array, backwards as we want the latest (last) launches
@@ -16,16 +17,29 @@
                 // with each push we increase counter by 1
                 counter++;
                 //we break the for loop, when the counter equals 3
-                if (counter == 3) {
+                if (counter === 3) {
                     break;
+
                 }
             }
+            // - Mission Name, Year, Date, Rocket Name and Type, Launch Site
+
+            let launchData =  latestLaunchesArr[0].launch_date_local.slice(0, 10);
+            console.log(
+                latestLaunchesArr[0],
+                latestLaunchesArr[0].mission_name,
+                latestLaunchesArr[0].launch_year,
+                launchData,
+                latestLaunchesArr[0].rocket.rocket_name,
+                latestLaunchesArr[0].rocket.rocket_type,
+                latestLaunchesArr[0].launch_site.site_name_long,
+                latestLaunchesArr[0].links.flickr_images[0]
+                )
         })
         .catch(function (err) {
             console.log(err.message);
         });
-
-    console.log(latestLaunchesArr)
 })();
+
 
 
