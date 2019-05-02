54d9ad2ffb3781bf146fc20380a8c8ca9cf624af
RadekKosiada <kosiadar@gmail.com>
2019-05-02 10:47:30 +0200

    Add comment & clean the code

--- script-vue.js
+++ script-vue.js
@@ -1,15 +1,18 @@
 var app = new Vue({
   el: '#app',
   data: {
+    //defining an empty array to push most recent launches
     lastLaunches: [],
+    //defining how many launches we want to display
     limitNumber: 3,
     counter: 0
   },
   mounted() {
+    //using axios as an alternative for fetch
     axios
       .get('https://api.spacexdata.com/v3/launches/past')
       .then(response => {
-        console.log("OK", response.data);
+        //for loop to get only most current launches
         for (let i = response.data.length - 1; i >= 0; i--) {
           this.lastLaunches.push(response.data[i]);
           this.counter++;
